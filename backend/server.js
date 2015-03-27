a37512dd3795052af9ba9382434e4c4216c31247
Mika Turunen <mika.turunen@ymail.com>
2015-03-27 15:02:25 +0200

    Static directory now works

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,19 @@
 var express = require("express");
 var app = express();
+var path = require("path");
 
 // TODO update front target
-app.use("/public", express.static(__dirname + "/frontend"));
+app.use("/public", express.static(path.join(__dirname, "..", "/frontend")));
+
 
 // Currently serve out everything like this -- dish it out like it means something
-app.get("*", function (req, res) {
-    res.send("Hello World!")
+app.get("/", function (req, res) {
+    res.send("Hello World!");
 });
 
 var server = app.listen(3000, function () {
-    var host = server.address().address
-    var port = server.address().port
+    var host = server.address().address;
+    var port = server.address().port;
 
-    console.log("Example app listening at http://%s:%s", host, port)
+    console.log("Example app listening at http://%s:%s", host, port);
 });
