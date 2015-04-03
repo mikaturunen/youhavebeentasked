f0345c74169191cb09a3fcf9a2ba3dcf096e16d7
Mika Turunen <mika.turunen@ymail.com>
2015-04-03 13:23:07 +0300

    Fixed some issues and minor refactor

--- backend/REST/tasks.js
+++ backend/REST/tasks.js
@@ -16,27 +16,39 @@ function getTaskList(req, res) {
 
 function newTask(req, res) {
     console.log(req.body);
-    res.json({
+    res.json({ 
         id: generateId(),
         title: "new task",
-        description: ". . .",
+        description: "...",
         events: [],
         done: false
     });
 }
 
 /** @type {Object[]} List of temp tasks before I move them to the db - should happen really soon now */
-var taskList = [ newTask(), newTask(), newTask() ];
-// Prefill them with information
-taskList[0].title = "Do the laundry";
-taskList[0].description = "Sort into loads, wash them, hang permanent press, dryer items and fold/ut away";
-taskList[0].events.push({ start: 1427241600 });
-taskList[1].title =  "Floor cleaning";
-taskList[1].description = "Dry mop/sweep, wet mop/scrub floors, vacuum, shampoo carpets, wax (If you are really ambitious!)";
-taskList[2].title = "Clean the yard";
-taskList[2].description = "Clean pool, camper/boat, garage/shed";
-taskList[2].events.push({ start: 1427241600, end: Date.now() });
-taskList[2].done = true;
+var taskList = [ 
+    { 
+        id: generateId(),
+        title: "Do the laundry",
+        description: "Sort into loads, wash them, hang permanent press, dryer items and fold/ut away",
+        events: [{ start: 1427241600 }],
+        done: false
+    }, 
+    { 
+        id: generateId(),
+        title: "Floor cleaning",
+        description: "Dry mop/sweep, wet mop/scrub floors, vacuum, shampoo carpets, wax (If you are really ambitious!)",
+        events: [],
+        done: false
+    }, 
+    { 
+        id: generateId(),
+        title: "Clean the yard",
+        description: "Clean pool, camper/boat, garage/shed",
+        events: [{ start: 1427241600, end: Date.now() }],
+        done: true
+    }, 
+];
 
 /**
  * Task routing module for all the REST interfaces that are associated with tasks
@@ -48,17 +60,13 @@ var taskRoutes = {
      * @param {Object} app Express application.
      */
     init: function(app) {
-        var prefix = "/api"
+        var prefix = "/api"; 
         [
             { route: prefix + "/tasks", callback: getTaskList, method: "get" },
             { route: prefix + "/tasks/new", callback: newTask, method: "post" }
-        ]
-        .forEach(function(router) {
+        ].forEach(function(router) {
             app[router.method](router.route, router.callback)
         });
-
-        // TODO move rest routes to their own location
-        app.get("/api/tasks", );
     }
 };
 
