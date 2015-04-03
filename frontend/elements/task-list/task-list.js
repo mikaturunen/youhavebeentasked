a9d7c65b45b5f9aca046ff388c1c84fec47508c9
Mika Turunen <mika.turunen@ymail.com>
2015-04-03 13:46:43 +0300

    Now the XHR response event is received and handled for the task. Broken still but moving to the right direction

--- frontend/elements/task-list/task-list.js
+++ frontend/elements/task-list/task-list.js
@@ -1,9 +1,18 @@
+
 Polymer({
     /** @type {boolean} Is loading content by default, display spinner */
     isLoadingContent: true,
 
     /** @type {Task[]} List of tasks. */
     tasks: [],
+
+    taskToAdd: {},
+
+    addTask: function(event) {
+        var task = JSON.parse(event.detail.xhr.response);
+        console.log(task);
+        this.tasks.push(task);
+    },
 
     /**
      * Creates a new task.
