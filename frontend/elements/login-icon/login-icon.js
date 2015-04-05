b61ae2080e860f7d1d32b53fddb450f044e9aefb
Mika Turunen <mika.turunen@ymail.com>
2015-04-05 21:40:43 +0300

    Created a new component for login-icon which controls the users login details

---
+++ frontend/elements/login-icon/login-icon.js
@@ -0,0 +1,50 @@
+
+Polymer("login-icon", {
+    /**
+     * Default icon for login-icon is account-box, can be changed through attribute 'icon='
+     * @type {String}
+     */
+    icon: "account-box",
+
+    /**
+     * Tries to login the user by reading the login credentials from the dialog inputs.
+     * Called when the user clicks "ok" on the Login dialog.
+     */
+    tryLoggingIn: function() {
+        console.log("Attempting to login the user.");
+        // Setting the user information in the POST data
+        // NOTE are you kidding me I have to manually stringify it for consumption with node? -argh-.
+        var stringifiedLoginDetails = JSON.stringify({ 
+            user: { 
+                username: this.$.Username.value, 
+                password: this.$.Password.value 
+            }
+        });
+
+        this.$.TryLoggingIn.body = stringifiedLoginDetails;
+        // Attempting to login with the content
+        this.$.TryLoggingIn.go();
+    },
+    
+    /** Opens Login dialog */
+    openLoginDialog: function() {
+        this.$.Login.open();
+    },
+    
+    /** Closes Login dialog */
+    closeLoginDialog: function() {
+        this.$.Login.close();
+    },
+
+    /** Called on error when the user attempts to login */
+    onLoginError: function(event) {
+        console.log("Error logging in the user.", event.detail.xhr.response);
+    },
+
+    /** Called on success when the user attempts to login */
+    onLoginSuccess: function(event) {
+        console.log("Success logging in the user.");
+        var loginDetails = JSON.parse(event.detail.xhr.response);
+        console.log(loginDetails);
+    }
+});
