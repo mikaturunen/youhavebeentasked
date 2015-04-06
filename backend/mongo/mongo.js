e10c3297e06df219c812c02bbb737bfdb810ba68
Mika Turunen <mika.turunen@ymail.com>
2015-04-06 11:16:59 +0300

    Created a super simple wrapper for mongo to hide connection and handling of collections.

---
+++ backend/mongo/mongo.js
@@ -0,0 +1,42 @@
+
+var Q = require("q");
+var MongoClient = require("mongodb").MongoClient
+
+/** @type {string} Urls that's build fron the application configuration and acts as the base configuration. */
+// TODO read from config + env variables -- now configured to default win mongodb port 
+var configuredUrl = "mongodb://localhost:27017";
+/** Database connection to MongoDB */
+var database;
+
+var database = {
+    /**
+     * Connects to the MongoDB database.
+     */
+    connect: function() {
+        var deferred = Q.defer();
+
+        MongoClient.connect(configuredUrl, function(error, mongoDatabase) {
+            if (error) {
+                console.log("Error connecting to mongodb:", error);
+                deferred.reject(error);
+                return;
+            }
+
+            console.log("Connected to mongodb database.");
+            database = mongoDatabase;
+            deferred.resolve(true);
+        });
+
+        return deferred.promise;
+    },
+
+    /**
+     * Gets a specified collection from MongoDB
+     * returns {Mongodb.Collection} Collection with the specified naem
+     */
+    collection: function(collection) {
+        return database.collection(collection);
+    }
+};
+
+module.exports = database;
