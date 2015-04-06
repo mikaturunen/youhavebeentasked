94541856c7eb3ea444cb03dc4cf9c0b88cb76caf
Mika Turunen <mika.turunen@ymail.com>
2015-04-06 10:00:27 +0300

    Fixed the session management - now users are properly serialized and deserialized.

--- backend/auth/authentication.js
+++ backend/auth/authentication.js
@@ -3,7 +3,7 @@ var express = require("express");
 var localStrategy = require("passport-local").Strategy;
 var cookieParser = require("cookie-parser");
 var bodyParser = require("body-parser");
-var cookieSession = require("cookie-session");
+var session = require("express-session");
 
 // TODO move away from plain text passwords and move them into the database asap when the testing is done 
 var users = {
@@ -27,8 +27,9 @@ function tryFindingUser(user, isPassportCall, done) {
     // TODO we are NOT going to use plain text passwords or other idiotic details like this - this is just for 
     //      quick testing that we know passport + local strategy are configured correctly and after that 
     //      we can safely swap in a proper authentication
-
+    console.log("TEST", user, isPassportCall);
     if (users[user.username] === undefined || (!isPassportCall && users[user.username].password !== user.password) ) {
+        console.log("INCORRECT USER OR PW");
         return done(null, false, { message: "Incorrect username or password" });
     }
 
@@ -37,6 +38,7 @@ function tryFindingUser(user, isPassportCall, done) {
         username: users[user.username].username,
     };
 
+    console.log("Found user:", user);
     return done(null, user);
 }
 
@@ -46,6 +48,7 @@ function tryFindingUser(user, isPassportCall, done) {
  */
 function getConfiguration() {
     return new localStrategy(function(username, password, done) {
+        console.log("local:", username, password);
         return tryFindingUser(
             {
                 username: username,
@@ -63,8 +66,7 @@ function getConfiguration() {
  * @param  {Function} done Middleware verification callback for Express and Passport combination
  */
 function userSerialization(user, done) {
-    console.log("Serializing user into passport");
-    delete user.password;
+    console.log("Serializing user into passport:", user);
     done(null, user);
 }
 
@@ -74,7 +76,7 @@ function userSerialization(user, done) {
  * @param  {Function} done Middleware verification callback for Express and Passport combination
  */
 function userDeSerialization(user, done) {
-    console.log("Deserializing user for passport");
+    console.log("Deserializing user for passport:", user);
     tryFindingUser(user, true, done);
 }
 
@@ -94,14 +96,19 @@ function setupPassport(app) {
  */
 function setupMiddlewaresRelatingToPassport(app) {
     // Configuring required express middlewares in place
+    app.use(cookieParser());
     app.use(bodyParser.json({
         extended: true
     }));
-    app.use(cookieParser());
-    app.use(cookieSession({ 
-        // TODO read this from environmental variable / configuration - for now it's ok to be here as we have
-        //      no real users or anything
-        secret: "~46:h.]^H#h5%)HgT0O5{Tfm97hw1Y"
+    app.use(session({
+        secret: "~46:h.]^H#h5%)HgT0O5{Tfm97hw1Y",
+        resave: false,
+        saveUninitialized: true,
+        cookie: { 
+            // NOTE https://github.com/expressjs/session
+            // For production vs testing for secure cookies on "Cookie options"
+            secure: false 
+        }
     }));
 
     // Express based application, we'll reroute the passport middlewares in place
@@ -114,24 +121,8 @@ function setupMiddlewaresRelatingToPassport(app) {
  * @param  {Express.Application} app Express application
  */
 function setupRoutes(app) {
-    app.post("/api/login", function(req, res) {
-        console.log("/api/login called - user attempting to login.");
-
-        tryFindingUser(req.body.user, false, function(error, user, message) {
-            // NOTE tryFindingUser is not wrapped in Promise as the callback is passport specific callback,
-            //      here we are just taking advantage of the existing behavior
-            if (error || user === false) {
-                console.log("Error finding user");
-                // These are the two conditions that are specified in passport documents for failing cases; when user
-                // is false or when there is an error message present. 
-                // Forbidden with message.
-                res.status(403).jsonp(message);
-            } else {
-                console.log("User found!", user);
-                // User allowed in.
-                res.json(user);           
-            } 
-        });
+    app.post("/api/login", passport.authenticate("local"), function(req, res) {
+        res.status(200).end();
     });
 }
 
