ac1f75c05da59a77d8af8f809f96e9bba9ddc65d
Mika Turunen <mika.turunen@ymail.com>
2015-04-07 19:23:53 +0300

    configuration module in place

--- backend/config/config.js
+++ backend/config/config.js
@@ -1,5 +1,18 @@
-var config = {
 
-};
+var configuration = require("./config.json");
+
+/** @type {Object} Exposed configuration object that will be built from config.json and environmental variables.  */
+var config = { };
+
+Object.keys(configuration).forEach(function(property) {
+    var value = configuration[property];
+
+    if (process.env[property] !== "" && process.env[property] !== null && process.env[property] !== undefined) {
+        value = process.env[property];
+    }
+
+    console.log("Config key value:", property, value);
+    config[property] = value;
+});
 
 module.exports = config;
